0d66b4fd14df895cc6a63beba306b38ed6f3facf
on99 <on99@users.noreply.github.com>
2016-11-04 00:48:17 +0800

    Add pending balance field to publisher and user

--- vendor/github.com/solefaucet/btcwall-rpc-model/publisher.go
+++ vendor/github.com/solefaucet/btcwall-rpc-model/publisher.go
@@ -7,23 +7,25 @@ import (
 
 // Publisher model
 type Publisher struct {
-	ID        int64     `db:"id" json:"id"`
-	Email     string    `db:"email" json:"email"`
-	Password  string    `db:"password" json:"-"`
-	Address   string    `db:"address" json:"address"`
-	Balance   int64     `db:"balance" json:"balance"`
-	CreatedAt time.Time `db:"created_at" json:"created_at"`
-	UpdatedAt time.Time `db:"updated_at" json:"updated_at"`
+	ID             int64     `db:"id" json:"id"`
+	Email          string    `db:"email" json:"email"`
+	Password       string    `db:"password" json:"-"`
+	Address        string    `db:"address" json:"address"`
+	Balance        int64     `db:"balance" json:"balance"`
+	PendingBalance int64     `db:"pending_balance" json:"pending_balance"`
+	CreatedAt      time.Time `db:"created_at" json:"created_at"`
+	UpdatedAt      time.Time `db:"updated_at" json:"updated_at"`
 }
 
 // MarshalJSON implements json marshaler
 func (p Publisher) MarshalJSON() ([]byte, error) {
 	return json.Marshal(map[string]interface{}{
-		"id":         p.ID,
-		"email":      p.Email,
-		"address":    p.Address,
-		"balance":    float64(p.Balance) / 1e8,
-		"created_at": p.CreatedAt,
-		"updated_at": p.UpdatedAt,
+		"id":              p.ID,
+		"email":           p.Email,
+		"address":         p.Address,
+		"balance":         float64(p.Balance) / 1e8,
+		"pending_balance": float64(p.PendingBalance) / 1e8,
+		"created_at":      p.CreatedAt,
+		"updated_at":      p.UpdatedAt,
 	})
 }
--- vendor/github.com/solefaucet/btcwall-rpc-model/user.go
+++ vendor/github.com/solefaucet/btcwall-rpc-model/user.go
@@ -7,20 +7,22 @@ import (
 
 // User model
 type User struct {
-	ID        int64     `db:"id" json:"id"`
-	Address   string    `db:"address" json:"address"`
-	Balance   int64     `db:"balance" json:"balance"`
-	CreatedAt time.Time `db:"created_at" json:"created_at"`
-	UpdatedAt time.Time `db:"updated_at" json:"updated_at"`
+	ID             int64     `db:"id" json:"id"`
+	Address        string    `db:"address" json:"address"`
+	Balance        int64     `db:"balance" json:"balance"`
+	PendingBalance int64     `db:"pending_balance" json:"pending_balance"`
+	CreatedAt      time.Time `db:"created_at" json:"created_at"`
+	UpdatedAt      time.Time `db:"updated_at" json:"updated_at"`
 }
 
 // MarshalJSON implements json marshaler
 func (u User) MarshalJSON() ([]byte, error) {
 	return json.Marshal(map[string]interface{}{
-		"id":         u.ID,
-		"address":    u.Address,
-		"balance":    float64(u.Balance) / 1e8,
-		"created_at": u.CreatedAt,
-		"updated_at": u.UpdatedAt,
+		"id":              u.ID,
+		"address":         u.Address,
+		"balance":         float64(u.Balance) / 1e8,
+		"pending_balance": float64(u.PendingBalance) / 1e8,
+		"created_at":      u.CreatedAt,
+		"updated_at":      u.UpdatedAt,
 	})
 }
