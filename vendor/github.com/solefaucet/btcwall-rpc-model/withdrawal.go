48488c4b6cfdb8317469733709ebd07b342536ea
on99 <on99@users.noreply.github.com>
2016-11-05 21:01:38 +0800

    Update rpc-model vendor

--- vendor/github.com/solefaucet/btcwall-rpc-model/withdrawal.go
+++ vendor/github.com/solefaucet/btcwall-rpc-model/withdrawal.go
@@ -1,6 +1,9 @@
 package rpcmodels
 
-import "time"
+import (
+	"encoding/json"
+	"time"
+)
 
 // withdrawal status
 const (
@@ -21,6 +24,20 @@ type UserWithdrawal struct {
 	CreatedAt     time.Time `db:"created_at" json:"created_at"`
 }
 
+// MarshalJSON implements json.Marshaler
+func (w UserWithdrawal) MarshalJSON() ([]byte, error) {
+	return json.Marshal(map[string]interface{}{
+		"id":             w.ID,
+		"user_id":        w.UserID,
+		"address":        w.Address,
+		"amount":         float64(w.Amount) / 1e8,
+		"transaction_id": w.TransactionID,
+		"status":         w.Status,
+		"created_at":     w.CreatedAt,
+		"updated_at":     w.UpdatedAt,
+	})
+}
+
 // PublisherWithdrawal model
 type PublisherWithdrawal struct {
 	ID            int64     `db:"id" json:"id"`
@@ -31,4 +48,18 @@ type PublisherWithdrawal struct {
 	TransactionID string    `db:"transaction_id" json:"tx_id"`
 	UpdatedAt     time.Time `db:"updated_at" json:"updated_at"`
 	CreatedAt     time.Time `db:"created_at" json:"created_at"`
+}
+
+// MarshalJSON implements json.Marshaler
+func (w PublisherWithdrawal) MarshalJSON() ([]byte, error) {
+	return json.Marshal(map[string]interface{}{
+		"id":             w.ID,
+		"publisher_id":   w.PublisherID,
+		"address":        w.Address,
+		"amount":         float64(w.Amount) / 1e8,
+		"transaction_id": w.TransactionID,
+		"status":         w.Status,
+		"created_at":     w.CreatedAt,
+		"updated_at":     w.UpdatedAt,
+	})
 }
