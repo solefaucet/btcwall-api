5ca5b240c013d55f3da583c4199355fc1622ab7c
on99 <on99@users.noreply.github.com>
2016-11-10 21:14:15 +0800

    Parse idCombination in json format

--- middlewares/id_parser.go
+++ middlewares/id_parser.go
@@ -1,70 +1,41 @@
 package middlewares
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
-	"strconv"
-	"strings"
 
 	"github.com/Sirupsen/logrus"
 	"github.com/gin-gonic/gin"
-	multierror "github.com/hashicorp/go-multierror"
-	"github.com/pkg/errors"
 	"github.com/solefaucet/btcwall-api/models"
 )
+
+type idPayload struct {
+	PublisherID int64  `json:"publisher_id"`
+	SiteID      int64  `json:"site_id"`
+	UserID      int64  `json:"user_id"`
+	TrackID     string `json:"track_id"`
+}
 
 // IDParserMiddleware parse publisher_id, site_id, user_id from query
 func IDParserMiddleware(key string) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		idCombination := c.Query(key)
-		publisherID, siteID, userID, err := parseIDCombination(idCombination)
-		if err != nil {
+		var payload idPayload
+		if err := json.Unmarshal([]byte(idCombination), &payload); err != nil {
+			logrus.WithFields(logrus.Fields{
+				"event":          models.LogEventParseIDCombination,
+				"id_combination": idCombination,
+				"error":          err.Error(),
+			}).Info("cannot parse id combination")
 			c.AbortWithError(http.StatusBadRequest, err)
 			return
 		}
 
-		c.Set("publisher_id", publisherID)
-		c.Set("site_id", siteID)
-		c.Set("user_id", userID)
+		c.Set("publisher_id", payload.PublisherID)
+		c.Set("site_id", payload.SiteID)
+		c.Set("user_id", payload.UserID)
+		c.Set("track_id", payload.TrackID)
 
 		c.Next()
 	}
-}
-
-func parseIDCombination(idCombination string) (publisherID, siteID, userID int64, err error) {
-	fields := strings.Split(idCombination, "_")
-	if len(fields) != 3 {
-		err = fmt.Errorf("invalid id combination format %v", idCombination)
-		return
-	}
-
-	errs := &multierror.Error{}
-	appendErrorIfNotNil := func(err error) {
-		if err != nil {
-			errs = multierror.Append(errs, nil)
-		}
-	}
-
-	publisherID, err = strconv.ParseInt(fields[0], 10, 64)
-	appendErrorIfNotNil(errors.Wrapf(err, "cannot parse publisher_id %v", fields[0]))
-
-	siteID, err = strconv.ParseInt(fields[1], 10, 64)
-	appendErrorIfNotNil(errors.Wrapf(err, "cannot parse site_id %v", fields[1]))
-
-	userID, err = strconv.ParseInt(fields[2], 10, 64)
-	appendErrorIfNotNil(errors.Wrapf(err, "cannot parse user_id %v", fields[2]))
-
-	err = errs.ErrorOrNil()
-	if err != nil {
-		logrus.WithFields(logrus.Fields{
-			"event":          models.LogEventParseIDCombination,
-			"id_combination": idCombination,
-			"publisher_id":   publisherID,
-			"site_id":        siteID,
-			"user_id":        userID,
-			"error":          err.Error(),
-		}).Info("cannot parse id combination")
-	}
-
-	return
 }
