26ec0b2518b27574d83d316fc9256e5d8a432ae2
on99 <on99@users.noreply.github.com>
2016-11-13 18:18:25 +0800

    Update create user log

--- handlers/v1/users.go
+++ handlers/v1/users.go
@@ -46,15 +46,15 @@ func (userHandler UserHandler) CreateUser() gin.HandlerFunc {
 			TrackID string `json:"track_id"`
 		}{}
 
+		if err := c.BindJSON(&payload); err != nil {
+			return
+		}
 		logrus.WithFields(logrus.Fields{
 			"event":    models.LogEventCreateUser,
 			"address":  payload.Address,
 			"track_id": payload.TrackID,
 		}).Debug("create user")
 
-		if err := c.BindJSON(&payload); err != nil {
-			return
-		}
 		payload.Address = strings.TrimSpace(payload.Address)
 		if !validateBitcoinAddress(payload.Address) {
 			c.AbortWithError(http.StatusBadRequest, fmt.Errorf("bitcoin address %s is invalid", payload.Address))
