9f9eee4868e16985c92509ca694ee2882cfa40ba
on99 <on99@users.noreply.github.com>
2016-11-29 23:25:08 +0800

    Update vendor

--- vendor/github.com/solefaucet/btcwall-rpc-model/withdrawal.go
+++ vendor/github.com/solefaucet/btcwall-rpc-model/withdrawal.go
@@ -15,14 +15,14 @@ const (
 
 // UserWithdrawal model
 type UserWithdrawal struct {
-	ID            int64     `db:"id" json:"id"`
-	UserID        int64     `db:"user_id" json:"user_id"`
-	Address       string    `db:"address" json:"address"`
-	Amount        int64     `db:"amount" json:"amount"`
-	Status        string    `db:"status" json:"status"`
-	TransactionID string    `db:"transaction_id" json:"tx_id"`
-	UpdatedAt     time.Time `db:"updated_at" json:"updated_at"`
-	CreatedAt     time.Time `db:"created_at" json:"created_at"`
+	ID            int64     `db:"id"`
+	UserID        int64     `db:"user_id"`
+	Address       string    `db:"address"`
+	Amount        int64     `db:"amount"`
+	Status        string    `db:"status"`
+	TransactionID string    `db:"transaction_id"`
+	UpdatedAt     time.Time `db:"updated_at"`
+	CreatedAt     time.Time `db:"created_at"`
 }
 
 // MarshalJSON implements json.Marshaler
@@ -41,14 +41,14 @@ func (w UserWithdrawal) MarshalJSON() ([]byte, error) {
 
 // PublisherWithdrawal model
 type PublisherWithdrawal struct {
-	ID            int64     `db:"id" json:"id"`
-	PublisherID   int64     `db:"publisher_id" json:"publisher_id"`
-	Address       string    `db:"address" json:"address"`
-	Amount        int64     `db:"amount" json:"amount"`
-	Status        string    `db:"status" json:"status"`
-	TransactionID string    `db:"transaction_id" json:"tx_id"`
-	UpdatedAt     time.Time `db:"updated_at" json:"updated_at"`
-	CreatedAt     time.Time `db:"created_at" json:"created_at"`
+	ID            int64     `db:"id"`
+	PublisherID   int64     `db:"publisher_id"`
+	Address       string    `db:"address"`
+	Amount        int64     `db:"amount"`
+	Status        string    `db:"status"`
+	TransactionID string    `db:"transaction_id"`
+	UpdatedAt     time.Time `db:"updated_at"`
+	CreatedAt     time.Time `db:"created_at"`
 }
 
 // MarshalJSON implements json.Marshaler
