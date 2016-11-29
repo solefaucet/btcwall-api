61782c02b72e01a1556ac21df7bbedc3c40afff6
on99 <on99@users.noreply.github.com>
2016-11-29 23:24:50 +0800

    Update kiwiwall transaction_id format

--- handlers/offerwalls/kiwiwall.go
+++ handlers/offerwalls/kiwiwall.go
@@ -1,6 +1,7 @@
 package offerwalls
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -12,10 +13,10 @@ import (
 func (h OfferwallHandler) KiwiwallCallback() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		payload := struct {
-			Amount        float64 `form:"amount" binding:"required"`
-			TransactionID string  `form:"trans_id" binding:"required"`
-			OfferName     string  `form:"offer_name"`
-			Status        int64   `form:"status" binding:"required,eq=1|eq=2"` // 1 success 2 reversal
+			Amount    float64 `form:"amount" binding:"required"`
+			OfferID   string  `form:"offer_id" binding:"required"`
+			OfferName string  `form:"offer_name"`
+			Status    int64   `form:"status" binding:"required,eq=1|eq=2"` // 1 success 2 reversal
 		}{}
 		if err := c.BindWith(&payload, binding.Form); err != nil {
 			logOfferwallCallback(models.OfferwallNameKiwiwall, c, err)
@@ -25,7 +26,7 @@ func (h OfferwallHandler) KiwiwallCallback() gin.HandlerFunc {
 		offer := offerFromContext(c)
 		offer.OfferName = payload.OfferName
 		offer.OfferwallName = models.OfferwallNameKiwiwall
-		offer.TransactionID = payload.TransactionID
+		offer.TransactionID = fmt.Sprintf("%v|%v", payload.OfferID, offer.UserID)
 		offer.Amount = int64(payload.Amount)
 
 		if err := h.handleOfferCallback(offer, payload.Status == 2); err != nil {
