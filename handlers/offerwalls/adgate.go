93c214369b15499129ec917b795452524ea9fe9a
on99 <on99@users.noreply.github.com>
2016-12-19 00:49:35 +0800

    Update adgate offer transaction id

--- handlers/offerwalls/adgate.go
+++ handlers/offerwalls/adgate.go
@@ -1,6 +1,7 @@
 package offerwalls
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -12,10 +13,10 @@ import (
 func (h OfferwallHandler) AdgateCallback() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		payload := struct {
-			Amount        float64 `form:"point_value" binding:"required"`
-			TransactionID string  `form:"tx_id" binding:"required"`
-			OfferName     string  `form:"offer_name"`
-			Status        int64   `form:"status" binding:"required,eq=1|eq=0"` // 1 success 0 chargeback
+			Amount    float64 `form:"point_value" binding:"required"`
+			OfferID   string  `form:"offer_id" binding:"required"`
+			OfferName string  `form:"offer_name"`
+			Status    int64   `form:"status" binding:"required,eq=1|eq=0"` // 1 success 0 chargeback
 		}{}
 		if err := c.BindWith(&payload, binding.Form); err != nil {
 			logOfferwallCallback(models.OfferwallNameAdgate, c, err)
@@ -25,7 +26,7 @@ func (h OfferwallHandler) AdgateCallback() gin.HandlerFunc {
 		offer := offerFromContext(c)
 		offer.OfferName = payload.OfferName
 		offer.OfferwallName = models.OfferwallNameAdgate
-		offer.TransactionID = payload.TransactionID
+		offer.TransactionID = fmt.Sprintf("%v|%v", payload.OfferID, offer.UserID)
 		offer.Amount = int64(payload.Amount)
 
 		if err := h.handleOfferCallback(offer, payload.Status == 0); err != nil {
